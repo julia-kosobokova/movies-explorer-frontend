c03eea5ec13bc881d6f544f5ba2aae14605ddcd5
Julia Kosobokova <jilika12@yandex.ru>
2023-06-13 12:21:12 +0300

    feat: сверстать footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,20 @@
 function Footer() {
     return (
       <footer className="footer">
-        <p>Учебный проект Яндекс.Практикум х BeatFilm.</p>
-        <p className="footer__copyright" lang="en">
-          &copy; {new Date().getFullYear()}. Mesto Russia
-        </p>
+        <p className="footer__top">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <div className="footer__bottom">
+          <p className="footer__copyright">
+            &copy; {new Date().getFullYear()}
+          </p>
+          <ul className="footer__links">
+            <li className="footer__list-item">
+              <a href="https://practicum.yandex.ru/" className="footer__link">Яндекс.Практикум</a>
+            </li>
+            <li className="footer__list-item" lang="en">
+              <a href="https://github.com/julia-kosobokova/" className="footer__link">Github</a>
+            </li>
+          </ul>
+        </div>
       </footer>
     );
   }
