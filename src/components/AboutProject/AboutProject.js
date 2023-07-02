7ff680a23583091d93f8464131d8440e925f415b
Julia Kosobokova <jilika12@yandex.ru>
2023-07-02 14:42:25 +0300

    feat: сверстать компонент AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,14 +4,26 @@ function AboutProject() {
     return (
         <section className="about-project" id="about">
             <SectionHeader>О проекте</SectionHeader>
-            <h3 className="about-project__title">Дипломный проект включал 5 этапов</h3>
-            <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-            <h3 className="about-project__title">На выполнение диплома ушло 5 недель</h3>
-            <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-            <p>1 неделя</p>
-            <p>Back-end</p>
-            <p></p>
-            <p>Front-end</p>
+            <div className="about-project__columns">
+                <div className="about-project__column">
+                    <h3 className="about-project__title">Дипломный проект включал 5 этапов</h3>
+                    <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                </div>
+                <div className="about-project__column">
+                    <h3 className="about-project__title">На выполнение диплома ушло 5 недель</h3>
+                    <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
+            </div>
+            <div className="about-project__table">
+                <div className="about-project__table-left">
+                    <div  className="about-project__table-title about-project__table-title_green">1 неделя</div>
+                    <p className="about-project__table-text">Back-end</p>
+                </div>
+                <div className="about-project__table-right">
+                    <div className="about-project__table-title">4 недели</div>
+                    <p className="about-project__table-text">Front-end</p>
+                </div>
+            </div>
         </section>
 );
 }
