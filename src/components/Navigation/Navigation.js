19100c02abcf53b8f0f080fa4c85b3919ca4598b
Julia Kosobokova <jilika12@yandex.ru>
2023-07-09 21:51:32 +0300

    feat: сделать выравнивение в Header

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,25 +5,6 @@ function Navigation(props) {
         <>
         <nav className={
             props.loggedIn
-            ? "nav__sign nav__sign_hidden"
-            : "nav__sign"
-        }>
-            <ul className="nav__links">
-                <li className="nav__list-item">
-                    <Link to="/signup" className="nav__link">
-                        <button className="nav__button">Регистрация</button>
-                    </Link>
-                </li>
-                <li className="nav__list-item">
-                    <Link to="/signin" className="nav__link nav__link_green">
-                        <button className="nav__button nav__button-green">Войти</button>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-
-        <nav className={
-            props.loggedIn
             ? "nav__movies"
             : "nav__movies nav__movies_hidden"
             }>
@@ -36,6 +17,25 @@ function Navigation(props) {
                 <li className="nav__list-item nav__list-item_movies">
                     <Link to="/saved-movies" className="nav__link">
                         <button className="nav__button">Сохраненные фильмы</button>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+
+        <nav className={
+            props.loggedIn
+            ? "nav__sign nav__sign_hidden"
+            : "nav__sign"
+        }>
+            <ul className="nav__links">
+                <li className="nav__list-item">
+                    <Link to="/signup" className="nav__link">
+                        <button className="nav__button">Регистрация</button>
+                    </Link>
+                </li>
+                <li className="nav__list-item">
+                    <Link to="/signin" className="nav__link nav__link_green">
+                        <button className="nav__button nav__button-green">Войти</button>
                     </Link>
                 </li>
             </ul>
