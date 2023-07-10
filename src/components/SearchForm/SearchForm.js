d649e38301e8cff4af7d1e571d5647d3227f4e46
Julia Kosobokova <jilika12@yandex.ru>
2023-07-10 13:45:52 +0300

    feat: сверстать компонент SeachForm при разрешениях больше 768

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,24 +3,26 @@ import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 function SearchForm() {
     return (
         <div className="search">
-            <div className="search__input">
-                <input
-                    type="text"
-                    name="movie"
-                    placeholder="Фильм"
-                    required
-                    className="search__input-text"
-                    minLength="2"
-                    maxLength="40"
-                />
-            
-                <button type="button" className="search__button" />
-            </div>
+            <div className="search__container">
+                <div className="search__input">
+                    <svg className="search__icon" alt="Поиск" />
+                    <input
+                        type="text"
+                        name="movie"
+                        placeholder="Фильм"
+                        required
+                        className="search__input-text"
+                        minLength="2"
+                        maxLength="40"
+                    />
+                
+                    <button type="button" className="search__button" />
+                </div>
             <div class="search__divider"></div>
             <FilterCheckbox />
             <div class="search__bottom"></div>
+            </div>
         </div>
-    )
-}
+    )}
 
 export default SearchForm;
