c55d3ee8430385887118d3c2519bb893ba511c7b
Julia Kosobokova <jilika12@yandex.ru>
2023-07-12 00:13:26 +0300

    refact: перенести блоки грида в header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,66 @@
 import { Link } from "react-router-dom";
 import logo from "../../images/header-logo.svg";
 import Navigation from "../Navigation/Navigation";
+import Menu from "../Menu/Menu";
+import { useState } from "react";
 
 function Header(props) {
+  let [menuIsVisible, setMenuIsVisible] = useState(false);
+
+  const showMenu = () => {
+    setMenuIsVisible(true);
+  };
+
+  const hideMenu = () => {
+    setMenuIsVisible(false);
+  };
   return (
     <header className="header">
-      <Link to="/" className="header__link">
+      <Link to="/" className="header__logo-link">
         <img src={logo} alt="Логотип проекта" className="header__logo" />
       </Link>
-      <Navigation loggedIn={props.loggedIn} activeLink={props.activeLink} />
+      <div className="header__center">
+        <Navigation loggedIn={props.loggedIn} activeLink={props.activeLink} />
+      </div>
+      <div className="header__right">
+        <nav
+          className={
+            props.loggedIn ? "header__sign header__sign_hidden" : "header__sign"
+          }
+        >
+          <ul className="header__links">
+            <li className="header__list-item">
+              <Link to="/signup" className="header__link">
+                <button className="header__button">Регистрация</button>
+              </Link>
+            </li>
+            <li className="header__list-item">
+              <Link to="/signin" className="header__link header__link_green">
+                <button className="header__button header__button-green">Войти</button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div
+          className={
+            props.loggedIn ? "header__profile" : "header__profile header__profile_hidden"
+          }
+        >
+          <Link to="/profile" className="header__link">
+            <button type="button" className="header__profile" />
+          </Link>
+        </div>
+
+        <div
+          className={
+            props.loggedIn ? "header__icon" : "header__icon header__icon_hidden"
+          }
+        >
+          <button type="button" className="header__icon" onClick={showMenu} />
+        </div>
+      </div>
+
+      <Menu isVisible={menuIsVisible} onClose={hideMenu} />
     </header>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,20 +1,8 @@
 import { Link } from "react-router-dom";
-import Menu from "../Menu/Menu";
-import { useState } from "react";
 
 function Navigation(props) {
-  let [menuIsVisible, setMenuIsVisible] = useState(false);
-
-  const showMenu = () => {
-    setMenuIsVisible(true);
-  };
-
-  const hideMenu = () => {
-    setMenuIsVisible(false);
-  };
   return (
-    <>
-      <div className="nav__center">
+    <div className="nav">
         <nav
           className={
             props.loggedIn ? "nav__movies" : "nav__movies nav__movies_hidden"
@@ -53,47 +41,7 @@ function Navigation(props) {
             props.loggedIn ? "nav__empty nav__empty_hidden" : "nav__empty"
           }
         ></nav>
-      </div>
-      <div className="nav__right">
-        <nav
-          className={
-            props.loggedIn ? "nav__sign nav__sign_hidden" : "nav__sign"
-          }
-        >
-          <ul className="nav__links">
-            <li className="nav__list-item">
-              <Link to="/signup" className="nav__link">
-                <button className="nav__button">Регистрация</button>
-              </Link>
-            </li>
-            <li className="nav__list-item">
-              <Link to="/signin" className="nav__link nav__link_green">
-                <button className="nav__button nav__button-green">Войти</button>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div
-          className={
-            props.loggedIn ? "nav__profile" : "nav__profile nav__profile_hidden"
-          }
-        >
-          <Link to="/profile" className="nav__link">
-            <button type="button" className="nav__profile" />
-          </Link>
-        </div>
-
-        <div
-          className={
-            props.loggedIn ? "nav__icon" : "nav__icon nav__icon_hidden"
-          }
-        >
-          <button type="button" className="nav__icon" onClick={showMenu} />
-        </div>
-      </div>
-
-      <Menu isVisible={menuIsVisible} onClose={hideMenu} />
-    </>
+    </div>
   );
 }
 
